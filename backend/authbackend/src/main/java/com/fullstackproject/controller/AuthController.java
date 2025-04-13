8050561f2cdeaff7775256d2f071949bb7238a98
yurelima016 <yurelimasouza@gmail.com>
2025-04-13 18:38:48 -0300

    applying token Jwt

--- backend/authbackend/src/main/java/com/fullstackproject/controller/AuthController.java
+++ backend/authbackend/src/main/java/com/fullstackproject/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.fullstackproject.controller;
 
 import com.fullstackproject.model.User;
 import com.fullstackproject.repository.UserRepository;
+import com.fullstackproject.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,14 +15,20 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @PostMapping("/login")
     public String login(@RequestBody User loginRequest) {
         Optional<User> userOptional = userRepository.findByEmail(loginRequest.getEmail());
 
         if (userOptional.isPresent()) {
             User user = userOptional.get();
+
             if (user.getPassword().equals(loginRequest.getPassword())) {
-                return "Login sucessfull!";
+                // Gera o token JWT
+                String token = jwtUtil.generateToken(user.getEmail());
+                return "Login successful! Token: " + token;
             }
         }
         return "Invalid email or password!";
